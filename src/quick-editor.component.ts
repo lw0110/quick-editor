ba36ed35ce7dbfc3c1746c95cc62e70f3c6f20f9
liwen <lw0110@gmail.com>
2016-07-16 12:04:38 +0800

    [LW] add geocode search in google map component to increase usability

--- src/quick-editor.component.ts
+++ src/quick-editor.component.ts
@@ -69,13 +69,30 @@ declare var google:any;
     <div class="form-group">
         <label class="col-sm-2 control-label">{{metadata.label}}</label>
         <div class="col-sm-10">
-            <div [attr.id]="'qe-map-' + id"></div>
+            <div [id]="'qe-map-floating-panel-' + id">
+                <input type="textbox" [(ngModel)]="geocodeStr">
+                <input type="button" value="Geocode" (click)="onSearchGeocode()">
+            </div>
+            <div [id]="'qe-map-' + id"></div>
         </div>
     </div>
     `,
     styles: [`
         div[id^="qe-map-"] {
             height: 300px;
+        }
+        
+        div[id^="qe-map-floating-panel-"] {
+            position: absolute;
+            top: 10px;
+            left: 25px;
+            height: auto;
+            z-index: 5;
+            background-color: #fff;
+            padding: 5px;
+            border: 1px solid #999;
+            text-align: center;
+            padding-left: 10px;
         }
     `],
     inputs: ['originalValue'],
@@ -85,6 +102,8 @@ declare var google:any;
 class QuickEditorGoogleMapComponent extends QuickEditorElement implements AfterViewInit {
     marker:any;
     map:any;
+    geocodeStr:string;
+    geocoder:any;
 
     ngAfterViewInit():any {
         let centerLatLng = {lat: this.currentValue[0], lng: this.currentValue[1]};
@@ -98,6 +117,8 @@ class QuickEditorGoogleMapComponent extends QuickEditorElement implements AfterV
             position: centerLatLng,
             map: this.map
         });
+
+        this.geocoder = new google.maps.Geocoder();
     }
 
     onEdit() {
@@ -123,6 +144,19 @@ class QuickEditorGoogleMapComponent extends QuickEditorElement implements AfterV
             this.map.setCenter(centerLatLng);
         }
 
+    }
+
+    onSearchGeocode() {
+        let currentMap = this.map;
+        let currentMarker = this.marker;
+        this.geocoder.geocode({'address': this.geocodeStr}, function(results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {
+                currentMap.setCenter(results[0].geometry.location);
+                currentMarker.setPosition(results[0].geometry.location);
+            } else {
+                alert('Geocode was not successful for the following reason: ' + status);
+            }
+        });
     }
 
 }
